e80f4b3f0d266428f98c33bb85c7361345a588b2
Vanita Patil <vanita.patil@outlook.com>
2024-02-22 17:09:14 +0530

    Made new class and changed the hardcoded password

--- src/main/java/org/example/Locators2.java
+++ src/main/java/org/example/Locators2.java
@@ -5,18 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
-
-import java.time.Duration;
-
 public class Locators2 {
     public static void main (String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:/Users/vanita_patil/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        String Password = getPassword(driver);
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.manage().window().maximize();
         String Name = "Vinnee";
         driver.findElement(By.id("inputUsername")).sendKeys(Name);
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.name("inputPassword")).sendKeys(Password);
         driver.findElement(By.className("signInBtn")).click();
 
         Thread.sleep(2000);
@@ -27,5 +25,26 @@ public class Locators2 {
         Assert.assertEquals(driver.findElement(By.tagName("h2")).getText(), "Hello " + Name +",");
         driver.findElement(By.xpath("//button[text()='Log Out']")).click();
         driver.close();
+    }
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        //Please use temporary password 'rahulshettyacademy' to Login.
+        String[] passwordArray = passwordText.split("'");
+        //0th index - Please use temporary password
+        //1st index - rahulshettyacademy' to Login
+
+        String[] passwordArray2 = passwordArray[1].split("'");
+        //0th index - rahulshettyacademy
+        //1st index - to Login
+        String password = passwordArray2[0];
+        System.out.println(password);
+        return password;
+
+
     }
 }
